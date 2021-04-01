f601ad4bdfc0775a24efa916e29f7da982e1b2f1
princy-32 <51436953+princy-32@users.noreply.github.com>
2021-04-01 10:12:36 +0530

    commit

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ var bodyparser = require('body-parser');
 
 var apiroutes = require ('./backend/routes/userRoutes');
 
+app.use(express.static(__dirname+'/dist/crudapp') );
+
 var cors = require('cors');
 
 var app =express();
 
-app.use(bodyparser.json()); 
-app.use(bodyparser.urlencoded({extended:false}));
+app.use(express.json()); 
+app.use(express.urlencoded({extended:false}));
 app.use(cors());
 
 app.use('/',apiroutes);
@@ -24,7 +26,7 @@ app.use((req , res, next )=>{
 })
 
 
-app.use(express.static(__dirname+'/dist/crudapp/index.html') );
+
 
 const port =process.env.PORT | 3000;
 
