2bc506fd5b7f48c3e9b39197332c65fe3d5cedb0
princy-32 <51436953+princy-32@users.noreply.github.com>
2021-04-02 10:08:04 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/projectDB",{useNewUrlParser:true })
+mongoose.connect("mongodb+srv://mydb:mydb123@cluster0.9m1zt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{useNewUrlParser:true })
 .then(()=>{
     console.log("Database connected successfully");     
 })
